bb16d7a385bbfde4eeb636269a539d03ff214f8f
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-23 10:22:55 -0400

    Character counter counts down and goes to negative

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,22 @@
 $(document).ready(function() {
 
   $('#tweet-text').on('input', function() {
-    const $tweetLength = $(this).val().length;
+
+    let $tweetLength = $(this).val().length;
     console.log($tweetLength);
+    const maxChars = 140;
+
+    let charsRemaining = (maxChars - $tweetLength);
+    console.log(charsRemaining);
+
+
+
+    //addClass "invalid" when over max characters
+    //invalid will be css coded red
+    //removeClass when under
+
+    //tweet-text.text(maxChar - $tweetLength)
+    //this?
   });
 
 });
