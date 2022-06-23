f54ee4e7ccc32dbc3664d80df10ea6958d6841ae
Aky Mac Donald <kotoko@Kotoko.local>
2022-06-23 18:49:36 -0400

    Working renderTweets function, more CSS edits on tweet containers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
-  console.log("Hi!");
 
   //takes a tweet object and returns a tweet <article> element containing the entire html structure of the tweet
-  const createTweetElement = function() {
+  const createTweetElement = function(tweetData) {
 
-    //creates a hardcoded tweet
-    const $tweet = $(`<article class="tweet-section">
+    let $tweet = $(`<article class="tweet-section">
         <header class="tweet-header">
           <div class="user-info">
             <img src="${tweetData.user.avatars}" width="64px" height="64px" class="profile-pic">
@@ -32,45 +30,49 @@ $(document).ready(function() {
 
         </footer>
         </article>
-`);
+     `);
 
     return $tweet;
   };
 
 
-  // //takes in an array of objects and appends each one to the #tweets-container.
-  // const renderTweets = function() {
-  //leverages the createTweetElement functions by passing the object to it, then using the returned jQuery object by appending it to the #tweets-container section
+  //takes in an array of objects and appends each one to the #tweets-container.
+  const renderTweets = function(data) {
 
-  //loops through tweets
-  //calls createTweetElement for each tweet
-  //takes return value and appends it to the tweet container
-  // };
-
-
-
-
-
-
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
+    for (const tweetData of data) {
+      const $tweet = $(createTweetElement(tweetData));
+      $('#tweets-container').append($tweet);
+    }
   };
 
-  const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  console.log("$tweet is: ", $tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  //driver code
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
 
-
+  renderTweets(data);
 
 });
